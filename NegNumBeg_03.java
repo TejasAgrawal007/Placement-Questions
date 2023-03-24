62372049db025a265d847627bab6753d4222f846
Tejas Agrawal <72118095+TejasAgrawal007@users.noreply.github.com>
2023-03-23 21:17:51 -0700

    Add files via upload

---
+++ NegNumBeg_03.java
@@ -0,0 +1,37 @@
+// Move all negative numbers to beginning and positive to end with constant extra space
+
+import java.util.*;
+
+public class NegNumBeg_03 {
+
+    static void reArrange(int arr[], int size) {
+        int j = 0, temp;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                if (i != j) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
+    }
+
+    static void printArray(int arr[], int size) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { -1, 2, -3, 4, 5, 6, -7, 8, 9 };
+        int size = arr.length;
+        printArray(arr, size);
+        reArrange(arr, size);
+        System.out.println();
+        printArray(arr, size);
+    }
+
+}
